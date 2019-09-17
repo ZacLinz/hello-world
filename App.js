7819a65bb3220e89f082fa9a903c43e8edeb7d75
ZacLinz <zaclinzmeyer@gmail.com>
2019-09-16 19:53:16 -0600

    mess around

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, Alert, Button, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = { text: '' };
+ }
+
+ // alert the user input
+alertMyText (input = []) {
+  Alert.alert(input.text);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ render() {
+   return (
+     <View style={{flex:1, justifyContent:'center'}}>
+       <TextInput
+         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+         onChangeText={(text) => this.setState({text})}
+         value={this.state.text}
+         placeholder='Type here ...'
+       />
+       <Text>You wrote: {this.state.text}</Text>
+       <Button
+        onPress={() => {
+          this.alertMyText({text: this.state.text});
+        }}
+        title="Press Me"
+        />
+     </View>
+   );
+ }
+}
